116c5e59d189a43f3fb4a681daf8601f4bd9a1b1
bhorsushmita <sushmita.bhor@sodelsolutions.com>
2022-07-07 16:02:46 +0530

    state useState in drop down

--- src/App.js
+++ src/App.js
@@ -1,32 +1,36 @@
-import { FormControl, MenuItem,Select } from '@material-ui/core';
+import { FormControl, MenuItem, Select } from '@material-ui/core';
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 function App() {
+  const [countries, setCountries] = useState(["USA", "UK", "INDIA"]);
   return (
     <div className="app">
- <div className="app__header"></div>
+      <div className="app__header">
+        <h1>COVID-19 tracker</h1>
+        <FormControl className='app__dropdown'>
+          <Select variant="outlined" value="abc">
+            {/* <MenuItem value="worldwide">worldwide</MenuItem>
+            <MenuItem value="worldwide">Option 2</MenuItem>
+            <MenuItem value="worldwide">Option 3</MenuItem>
+            <MenuItem value="worldwide">Option 4</MenuItem> */}
 
-    
-     <h1>COVID-19 tracker</h1>
-    <FormControl className='app__dropdown'>
-      <Select variant="outlined" value="abc">
-<MenuItem value="worldwide">worldwide</MenuItem>
-<MenuItem value="worldwide">Option 2</MenuItem>
-<MenuItem value="worldwide">Option 3</MenuItem>
-<MenuItem value="worldwide">Option 4</MenuItem>
-      </Select>
-    </FormControl>
-    {/*Header */}
-    {/*Title + Select input dropdown field */}
+            {
+              countries.map((country) => (<MenuItem value={country}>{country}</MenuItem>))
+            }
+          </Select>
+        </FormControl>
+      </div>
+      {/*Header */}
+      {/*Title + Select input dropdown field */}
 
 
-    {/* InfoBoxs */}
-    {/* InfoBoxs */}
-    {/* InfoBoxs */}
-    
+      {/* InfoBoxs */}
+      {/* InfoBoxs */}
+      {/* InfoBoxs */}
 
-    {/* Table */}
-    {/* Map */}
+
+      {/* Table */}
+      {/* Map */}
     </div>
   );
 }
